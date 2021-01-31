db946cb2c1428bd693b84e3b9ec33981bc6e1d72
avdorzhiev <avdorzhiev99@gmail.com>
2021-02-01 06:20:30 +0700

    db 3.0

--- routes/movies.js
+++ routes/movies.js
@@ -1,19 +1,38 @@
+const { json } = require('body-parser');
 const express = require('express');
 const { validate } = require('jsonschema');
 const { request } = require('request');
+const fetch = require('node-fetch')
 const rp = require('request-promise');
- 
+
 const router = express.Router();
 
-router.get('/movies', (req, res, next) => {
-    rp('https://www.omdbapi.com/?s='+'Iron'+'&apikey=thewdb')
-    .then(response => res.json({status: 'OK', movies: response}));
-});
+router.get('/movies/:s', (req, res, next) => {
+    const movies = [];
+    const { s } = req.params
+    rp('https://www.omdbapi.com/?s=' + s + '&apikey=thewdb')
+        .then(response => JSON.parse(response))
+        .then(results => {
+            const promises = [];
 
-router.get('/movies/:id', (req, res, next) => {
-    const { id } = req.params;
-    rp('https://www.omdbapi.com/?i='+results['Search'][id]['imdbID']+'&apikey=thewdb')
-    .then(response => res.json({status: 'OK', movies: response}));
+            for (let i = 0; i < results['Search'].length; i++) {
+                promises.push(rp('https://www.omdbapi.com/?i=' + results['Search'][i]['imdbID'] + '&apikey=thewdb')
+                    .then(data => {
+                        movies.push(data)
+                    }))
+            }
+            Promise.all(promises).then(() => {
+                console.log(movies)
+                res.json({ status: 'OK', movies: movies });
+            })
+        })
+})
+
+router.get('/movies/id/:id', (req, res, next) => {
+    const { id } = req.params
+    console.log('search')
+    rp('https://www.omdbapi.com/?i=' + id + '&apikey=thewdb')
+        .then(response => res.json({ status: 'OK', movies: response }));
 });
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -7,44 +7,66 @@ const db = require('../db/db');
 const users = db.get('users');
 
 router.get('/users', (req, res, next) => {
-    res.json({status: 'OK', users: users});
+    res.json({ status: 'OK', users: users });
 });
 
 router.get('/users/:id', (req, res, next) => {
-    const { id } = req.params; 
+    const { id } = req.params;
     const user = users.find(user => String(user.id) === id);
-    
-    res.json({status: 'OK', user});
+
+    res.json({ status: 'OK', user });
 });
 
 router.post('/users', (req, res, next) => {
-    const count = users.length;
+    const count = users.size;
+    console.log('c', users)
+
+    // let asd = users.map(el => el)
+    // console.log('users', asd)
 
     const { body } = req;
     const taskShecma = {
         type: 'object',
         properties: {
+            id: { type: 'string' },
             username: { type: 'string' },
             password: { type: 'string' },
         },
-        required: ['username', 'password'],
+        required: ['id', 'username', 'password'],
         additionalProperties: false
     };
 
     const validationResult = validate(body, taskShecma);
-    if (!validationResult.valid){
+    if (!validationResult.valid) {
         return next(new Error('INVALID_JSON_OR_API_FORMAT'));
     }
-
-    const newUser = { id: count+1, username: body.username, password: body.password };
+    console.log('count', count)
+    const newUser = { id: body.id, username: body.username, password: body.password };
+    const user = users.find(user => String(user.id) === id);
     try {
-        db.get('users') 
-          .push(newUser)
-          .write();
+        db.get('users')
+            .push(newUser)
+            .write();
     } catch (error) {
         throw new Error(error);
     }
-    res.json({status: 'OK', newUser: newUser});
+    res.json({ status: 'OK', newUser: newUser });
+})
+
+router.patch('/users/:id', (req, res, next) => {
+    const { id } = req.params;
+
+    const editedTask = db
+        .get('users')
+        .find({ id })
+        .assign(req.body)
+        .value();
+        
+    db.write();
+
+    console.log(req.body)
+
+    res.json({ status: 'OK', data: editedTask });
 })
 
 module.exports = router;
